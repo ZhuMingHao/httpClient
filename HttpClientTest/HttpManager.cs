dbd65a729a198f3bd69e30d6e724196889a1b3ba
ZhuMingHao <799573337@qq.com>
2016-10-28 18:17:39 +0800

    Add project files.

---
+++ HttpClientTest/HttpManager.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using System.Collections;
+using Windows.Storage.Streams;
+using System.Threading;
+using System.Net;
+using Windows.Storage;
+
+namespace HttpClientTest
+{
+    public class HttpManager
+    {
+
+        public async void SendGetMethod(string url, Action<HttpResponseMessage> resopnse)
+        {
+            HttpClient client = new HttpClient();
+            var headers = client.DefaultRequestHeaders;
+            string header = "";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                // throw new Exception("Invalid header value: " + header);
+            }
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            Uri requestUri = new Uri(url);
+            try
+            {
+
+                httpResponse = await client.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                resopnse(httpResponse);
+            }
+            catch (Exception ex)
+            {
+                Debug.Write("Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message);
+            }
+
+        }
+        public async void SendPostMethod(string url, StorageFile file, Dictionary<string, string> param, Action<HttpMultipartFormDataContent> FileContent, Action<string> response)
+        {
+            HttpClient client = new HttpClient();
+            var headers = client.DefaultRequestHeaders;
+            string header = "";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            Uri requestUri = new Uri(url);
+            var content = new HttpMultipartFormDataContent();
+
+            foreach (var item in param)
+            {
+                content.Add(new HttpStringContent(item.Value),item.Key);
+
+            }
+            //var fileContent = new HttpStreamContent(await file.OpenAsync(FileAccessMode.Read));
+            //fileContent.Headers.Add("Content-Type", "multipart/form-data");
+            //传递出去
+            FileContent(content);
+            try
+            {
+                httpResponse = await client.PostAsync(requestUri, content);
+                response(await httpResponse.Content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+            }
+        }
+        public async void SendPostMethod(string url, StorageFile file, Dictionary<string, string> param, Func<HttpMultipartFormDataContent, Task> FileContent, Action<string> response)
+        {
+            HttpClient client = new HttpClient();
+            var headers = client.DefaultRequestHeaders;
+            string header = "";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                // throw new Exception("Invalid header value: " + header);
+            }
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                //  throw new Exception("Invalid header value: " + header);
+            }
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            Uri requestUri = new Uri(url);
+            var content = new HttpMultipartFormDataContent();
+
+            foreach (var item in param)
+            {
+                content.Add(new HttpStringContent(item.Value), item.Key);
+
+            }
+            //var fileContent = new HttpStreamContent(await file.OpenAsync(FileAccessMode.Read));
+            //fileContent.Headers.Add("Content-Type", "multipart/form-data");
+            //传递出去
+
+            // FileContent.Invoke(content);
+            await FileContent(content);
+
+            try
+            {
+                httpResponse = await client.PostAsync(requestUri, content);
+                response(await httpResponse.Content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+            }
+        }
+   
+        public async void SendPostMethod(string url, Dictionary<string, string> param, Action<string> response)
+        {
+            HttpClient client = new HttpClient();
+            var headers = client.DefaultRequestHeaders;
+            string header = "";
+            headers.Add("", "");
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            Uri requestUri = new Uri(url);
+
+            var content = new HttpFormUrlEncodedContent(param);
+            try
+            {
+                httpResponse = await client.PostAsync(requestUri, content);
+
+                response(await httpResponse.Content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ HttpClientTest/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace HttpClientTest
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {  //https://api.weibo.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&response_type=code&redirect_uri=YOUR_REGISTERED_REDIRECT_URI
+
+        static string Key = "4017369158";
+        static string Secret = "fb016a546a460d60e7fe8d6bb0929fff";
+        static string redirect = "http://www.baidu.com";
+        string code;
+        public MainPage()
+        {   
+            this.InitializeComponent();
+            string url = string.Format("https://api.weibo.com/oauth2/authorize?client_id={0}&response_type=code&redirect_uri={1}",Key,"http://www.baidu.com");
+            MyWebView.Source =new Uri(url);
+        }
+
+        private  void WebView_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            string temp = e.Uri.ToString();
+            Debug.Write(temp);
+            string[] strArr =temp.Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+            code = strArr.Last<string>();
+            if(code != null)
+            {
+                HttpManager manager = new HttpManager();
+                string two = string.Format("https://api.weibo.com/oauth2/access_token?client_id={0}&client_secret={1}&grant_type=authorization_code&redirect_uri={2}&code={3}", Key, Secret, redirect, code
+   );
+               //manager.SendGetMethod("http://pic33.nipic.com/20130916/3420027_192919547000_2.jpg", (response) =>
+               // {
+               //     Debug.Write(response);
+               // });
+               
+            }
+           
+        }
+       
+    }
+}
